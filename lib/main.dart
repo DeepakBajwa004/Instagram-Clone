8cfca9d83fbcd70bdd3f9af1ffaca40ed20c279d
MasoomJaat <masoomjaat860@gmail.com>
2024-07-05 23:36:29 +0530

    Instagram Clone

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
+import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor
+      ),
+      home: ResponsiveLayout(
+        webScreenLayout: WebScreenLayout() ,
+        mobileScreenLayout: MobileScreenLayout(),),
+    );
+  }
+}
